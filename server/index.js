6cb868ccdeb777519318ee3e4a128cd1372c4b22
pratiksha950 <salunkepratiksha950@gmail.com>
2026-02-13 10:18:58 +0530

    model folder created:

--- server/index.js
+++ server/index.js
@@ -9,14 +9,18 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-
-
 const PORT=8080;
 
 //health Routes
 app.get("/",(req,res)=>{
     console.log("welcome to server");
+    res.send("welcome to server");
 }) 
+
+app.get("/health",(req,res)=>{
+     res.send("welcome to server");
+}) 
+
 
 
 app.listen(PORT,()=>{
