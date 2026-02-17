2faaf13c97cc1e820ae6cb353c9e5ef0113617c1
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-02-17 18:06:24 +0530

    valentine page data added

---
+++ client/src/configs/valentinecarddata.js
@@ -0,0 +1,79 @@
+
+import { generatePlaceholderImage } from "../utils/placeholderImage";
+const products = [
+    {
+      id: 201,
+      name: "Love Letter Personalized Card",
+      description: "Express your feelings with a custom card",
+      price: 399,
+      originalPrice: 499,
+      discount: 20,
+      image: generatePlaceholderImage("Love Card", "#fee2e2"),
+    },
+    {
+      id: 202,
+      name: "Couple Photo Locket",
+      description: "Precious moments in a beautiful locket",
+      price: 699,
+      originalPrice: 899,
+      discount: 22,
+      image: generatePlaceholderImage("Couple Locket", "#fee2e2"),
+    },
+    {
+      id: 203,
+      name: "Custom Love Story Book",
+      description: "Our memories in a beautiful photo book",
+      price: 999,
+      originalPrice: 1299,
+      discount: 23,
+      image: generatePlaceholderImage("Love Book", "#fee2e2"),
+    },
+    {
+      id: 204,
+      name: "Personalized Valentine Mug Set",
+      description: "Matching mugs with your couple photo",
+      price: 799,
+      originalPrice: 999,
+      discount: 20,
+      image: generatePlaceholderImage("Valentine Mugs", "#fee2e2"),
+    },
+    {
+      id: 205,
+      name: "Custom Love Pillow Cover",
+      description: "Romantic pillow with your couple photo",
+      price: 599,
+      originalPrice: 799,
+      discount: 25,
+      image: generatePlaceholderImage("Love Pillow", "#fee2e2"),
+    },
+    {
+      id: 206,
+      name: "Personalized Valentine Canvas",
+      description: "Beautiful canvas with a heartfelt message",
+      price: 899,
+      originalPrice: 1199,
+      discount: 25,
+      image: generatePlaceholderImage("Valentine Canvas", "#fee2e2"),
+    },
+    {
+      id: 207,
+      name: "Custom Couple Portrait Sketch",
+      description: "Hand-drawn style portrait of you both",
+      price: 799,
+      originalPrice: 999,
+      discount: 20,
+      image: generatePlaceholderImage("Couple Sketch", "#fee2e2"),
+    },
+    {
+      id: 208,
+      name: "Personalized Valentine Magnetic Photo",
+      description: "Cute magnetic photo for your fridge",
+      price: 249,
+      originalPrice: 349,
+      discount: 29,
+      image: generatePlaceholderImage("Love Magnet", "#fee2e2"),
+    },
+  ];
+
+
+export default products;
