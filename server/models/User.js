519be6d7914faa405f161c062ad64c15ace10c7f
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-02-17 23:33:42 +0530

    user profile scema inserted

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+import {Schema,model} from "mongoose";
+
+const userSchema=new Schema({
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    mobile:{
+        type:String,
+    },
+    city:{
+        type:String,
+    },
+    country:{
+        type:String,
+    },
+    pincode:{
+        type:String,
+    },
+    whatsappNumber:{
+        type:String,
+    },
+    address:{
+        type:String,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    profilePic:{
+    type:String,
+},
+
+
+},{timestamps:true})
+
+const User=model("User",userSchema);
+
+export default User
