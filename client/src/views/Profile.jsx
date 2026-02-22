6bf12b560ec27d41fc5693c5eb027ccece7497eb
pratiksha950 <salunkepratiksha950@gmail.com>
2026-02-22 12:04:58 +0530

    rontend Code Me URL Replace kiya

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -37,7 +37,7 @@ function Profile() {
 
   const authenticator = async () => {
     try {
-      const response = await fetch("http://localhost:8080/auth");
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth`);
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(`Request failed with status ${response.status}: ${errorText}`);
