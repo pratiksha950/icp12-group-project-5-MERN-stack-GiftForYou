e2428ecd9bc34055b0756286b6aecbe2cd8e676c
pratiksha950 <salunkepratiksha950@gmail.com>
2026-02-22 20:23:18 +0530

    require changes added

--- client/src/utils.jsx
+++ client/src/utils.jsx
@@ -30,7 +30,7 @@ const addToCart = async (item) => {
 
     try {
         const response = await axios.post(
-            `${import.meta.env.VITE_API_URL}/cart`,
+            `http://localhost:8080/cart`,
             item,
             {
                 headers: {
